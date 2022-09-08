9aa5ba229b59bb7e6e73476f7d3b5414bdbc8f71
timishot <timidotcom58@gmail.com>
2022-09-08 13:26:28 -0700

    cp

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *
+ *main -prints out a string to stdout.
+ *
+ *Description: using the main function
+ *
+ *this program prints "Programming is like building a multilingual puzzle
+ *
+ * return:0 if success
+ *
+ *      
+ *
+ */
+
+int main(void)
+{
+
+        puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+
+}
