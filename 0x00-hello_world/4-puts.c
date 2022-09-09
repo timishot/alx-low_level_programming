9d553832d957dbd7829b48f912de3adc87790a5b
timishot <timidotcom58@gmail.com>
2022-09-08 20:03:45 -0700

    noe

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 
 /**
- *
  *main -prints out a string to stdout.
- *
  *Description: using the main function
- **this program prints "Programming is like building a multilingual puzzle
- **return:0 if success
+ *this program prints "Programming is like building a multilingual puzzle
+ *return:0 if success
  */
 int main(void)
 {
