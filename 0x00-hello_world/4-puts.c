422d5b0332dbd291a3073bc0a133a5c116d60222
timishot <timidotcom58@gmail.com>
2022-09-08 19:48:11 -0700

    add

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,16 +5,11 @@
  *main -prints out a string to stdout.
  *
  *Description: using the main function
- *
- *this program prints "Programming is like building a multilingual puzzle
- *
- * return:0 if success
- *
- *      
- *
+ **this program prints "Programming is like building a multilingual puzzle
+ **return:0 if success
  */
 int main(void)
 {
-        puts("Programming is like building a multilingual puzzle\n");
-	return 0;
+        puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
