d60079adaf78db60851654363715cac92cb377ac
timishot <timidotcom58@gmail.com>
2022-09-08 21:27:22 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main- print exactly "and that piece of art is useful"
+ * - Dora korpar, 2015-10-19,
+ * followed by a new line,to the standard error.
+ * Return always 0 (success)
+ */
+int main(void)
+{
+	write(2,"and that piece of art is useful\" - Dora, 2015-10-19\n",59);
+	return (1);
+}
