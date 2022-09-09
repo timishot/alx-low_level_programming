f7ab3c6e644505456878da0e2fc0fe3fdb54b442
timishot <timidotcom58@gmail.com>
2022-09-08 20:07:40 -0700

    now

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,8 @@
 /**
  *main -prints out a string to stdout.
  *Description: using the main function
- *this program prints "Programming is like building a multilingual puzzle
- *return:0 if success
+ *this program prints "Programming is like building a multilingual puzzle"
+ *return: 0 if success
  */
 int main(void)
 {
