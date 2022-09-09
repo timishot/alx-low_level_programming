2a4aa01df95abbd901b94528708f3df06c1d3bee
timishot <timidotcom58@gmail.com>
2022-09-08 21:43:13 -0700

    n

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,8 @@
  */
 int main(void)
 {
-        write(2, "and that piece of art is useful\" - Dora, 2015-10-19\n",59);
+        char str1[]="and that piece of art is useful\" - Dora, 2015-10-19\n";
+
+	write(2,str1,59);
 	return (1);
 }
