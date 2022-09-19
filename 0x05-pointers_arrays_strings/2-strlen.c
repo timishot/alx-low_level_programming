3d2770b96f0229b15ce8929fbbeeec8d5b5a3df5
Timilehin Adeniran <101478000+timishot@users.noreply.github.com>
2022-09-19 16:48:28 +0100

    Create 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *_strlen- counts string exccept \0
+ *@s: checked
+ *Return: i
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; ++i)
+	{
+		;
+	}
+	return (i);
+
+}
