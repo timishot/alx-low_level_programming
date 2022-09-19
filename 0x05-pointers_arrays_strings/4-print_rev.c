156023c5365579d1ed70deafdc6c5e1af22dc725
Timilehin Adeniran <101478000+timishot@users.noreply.github.com>
2022-09-19 16:50:10 +0100

    Create 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *print_rev- prints in reverse
+ *@s: checked
+ *
+ *Return: nothing
+ */
+void print_rev(char *s)
+{
+
+	int i, len;
+
+	i = 0;
+
+	len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	for (i = len - 1; i >= 0; i--)
+	{
+		_putchar(*(s + i));
+	}
+	_putchar('\n');
+}
