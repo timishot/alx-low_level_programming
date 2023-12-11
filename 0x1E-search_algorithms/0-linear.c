e833f2e4276d995570d9a6b87ea72af2a48c96a9
Timishot <timidotcom58@gmail.com>
2023-12-11 09:22:58 +0100

    Create linear search algorithm

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * linear_search - function that searches for a value in an array
+ *  of integers using the Linear search algorithm
+ *  @array: a pointer to the first element of the array to search
+ *  @size: size is the number of elements in array
+ *  @value: value is not present in array
+ *  Return: Value else (-1) 
+ */
+
+int linear_search(int *array, size_t size, int value){
+	size_t i;
+	for (i = 0; i < size; i++){
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
+}
