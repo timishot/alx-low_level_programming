f0d6354c685beaab3b0572fc0b5800179ed0cf22
Timishot <timidotcom58@gmail.com>
2023-12-13 13:22:50 +0100

    done

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -17,7 +17,7 @@ int linear_search(int *array, size_t size, int value)
 
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 		if (array[i] == value)
-			return (i);
+			return ((int)i);
 	}
 
 
